3c28450d61cd88b5bb3debc59c0de47f9a93ddc1
Shuaiwei.Yu <shuaiwei.yu@le-xcom.net>
2024-02-20 14:30:45 +0100

    clock

--- src/components/clockSetting.vue
+++ src/components/clockSetting.vue
@@ -3,8 +3,15 @@
 
 <template>
 
-  <div class="actions">
-    <button class="delete" @click="getCurrentTime">clock</button>
+<div>
+    <div v-if="!timerOn">
+      <input type="number" v-model="workingMinutes" min="1" placeholder="输入分钟数">
+      <button @click="startTimer">开始番茄钟</button>
+    </div>
+    <div v-else>
+      <div>{{ displayTime }}</div>
+      <button @click="resetTimer">重置番茄钟</button>
+    </div>
   </div>
 
 </template>
@@ -14,24 +21,76 @@ export default {
   name: "clockSetting",
   data() {
     return {
-      workingTimePlanned: 45,
-      breakTimePlanned: 15,
-      currentTime: "",
+      workingMinutes: 25,
+      workingSeconds: 0,
+      isWorking : true,
+      restingMinutes: 10,
+      restingSeconds: 0,
+      isResting: false,
+      timerOn: false,
+      timer: null
+    };
+  }, computed: {
+    displayTime() {
+      if (this.isWorking) {
+        return `${this.padZero(this.workingMinutes)}:${this.padZero(this.workingSeconds)}`;
+      } else{
+        return `${this.padZero(this.restingMinutes)}:${this.padZero(this.restingSeconds)}`;
+      }
+      
     }
-  }, methods: {
-    getCurrentTime() {
-      let date = new Date();
-      let hours = date.getHours();
-      let minutes = date.getMinutes();
-      let seconds = date.getSeconds();
-      console.log(`${hours}:${minutes}:${seconds}`);
+  },
+  methods: {
+    startTimer() {
+      this.timerOn = true;
+      this.timer = setInterval(this.countDown, 1000);
+    },
+    countDown() {
+      if (this.isWorking) {
+        if (this.workingMinutes === 0 && this.workingSeconds === 0) {
+          // clearInterval(this.timer);
+          // this.timerOn = false;
+          // alert('番茄钟时间到！');
+          // return;
+          this.isWorking = false
+          this.isResting = true
+        }
+        if (this.workingSeconds === 0) {
+          this.workingMinutes--;
+          this.workingSeconds = 59;
+        } else {
+          this.workingSeconds--;
+        }
+      } else {
+        if (this.restingMinutes === 0 && this.restingSeconds === 0) {
+          // clearInterval(this.timer);
+          // this.timerOn = false;
+          // alert('番茄钟时间到！');
+          // return;
+          this.isWorking = true
+          this.isResting = false
+        }
+        if (this.restingSeconds === 0) {
+          this.restingMinutes--;
+          this.restingSeconds = 59;
+        } else {
+          this.restingSeconds--;
+        }
+      }
+    },
+    resetTimer() {
+      clearInterval(this.timer);
+      this.timerOn = false;
+      this.workingMinutes = 25;
+      this.workingSeconds = 0;
+    },
+    padZero(num) {
+      return num < 10 ? '0' + num : num;
     }
   }
 }
 </script>
 
 <style scoped>
-.actions .delete {
-  background-color: var(--danger);
-}
+
 </style>
